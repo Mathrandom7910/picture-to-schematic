422eec482914f70d3083852358447a1f6aa02419
Justgamer101 <79098706+Justgamer101@users.noreply.github.com>
2021-11-17 16:34:03 -0500

    Add files via upload

---
+++ imagetoschematic/getbrightnessval.py
@@ -0,0 +1,4 @@
+
+
+def get_bright(r, g, b):
+    return sum([r, g, b]) / 3
---
+++ imagetoschematic/imagereader.py
@@ -0,0 +1,23 @@
+from PIL import Image as Img
+from nbtschematic import SchematicFile
+from getbrightnessval import get_bright
+
+
+def get_image_pixels(path, dark, light):
+    im = Img.open(path)
+    size = im.size
+    sizex = size[0]
+    sizey = size[1]
+    pix = im.load()
+    sf = SchematicFile(shape=(1, sizey, sizex))
+
+    for i in range(sizex):
+        for j in range(sizey):
+            pin = pix[i, j]
+            if get_bright(pin[0], pin[1], pin[2]) < (256 / 2):
+                sf.blocks[0, j, i] = dark
+                print("set block " + str(i) + " 1 " + str(j) + " to dark block")
+            else:
+                sf.blocks[0, j, i] = light
+                print("set block " + str(i) + " 1 " + str(j) + " to light block")
+    return sf
